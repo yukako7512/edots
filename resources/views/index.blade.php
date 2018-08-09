b28907a1cf16778cfcd9846bf3a55f6b73d4e9bf
Nishiyama <yukako.nishiyama@rakuten.com>
2018-08-09 14:44:30 +0900

    jquery

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,7 +25,7 @@
 
 }
 </style>
-<script src="http://code.jquery.com/jquery-2.2.4.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script>
 (function($){
   var settings = {};
